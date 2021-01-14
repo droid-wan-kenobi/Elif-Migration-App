5b01e57d88ab729a8ea900185efda639b46ec014
Elif Bilgin <elifbilgin@google.com>
2021-01-14 11:02:31 -0800

    First draft of using RecyclerView

--- app/src/main/java/com/example/elifmigrationapp/MainActivity.kt
+++ app/src/main/java/com/example/elifmigrationapp/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.elifmigrationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.room.Room
 import com.example.elifmigrationapp.adapter.ItemAdapter
 import com.example.elifmigrationapp.data.Datasource
 
@@ -14,8 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val myDataset = Datasource().loadDataFromDatabase()
+    override fun onStart() {
+        super.onStart()
+        val db = Room.inMemoryDatabaseBuilder(
+            applicationContext,
+            MusicDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        val myDataset = Datasource().loadDataFromDatabase(db)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
--- app/src/main/java/com/example/elifmigrationapp/data/Datasource.kt
+++ app/src/main/java/com/example/elifmigrationapp/data/Datasource.kt
@@ -1,13 +1,22 @@
 package com.example.elifmigrationapp.data
 
-import com.example.elifmigrationapp.R
+import android.content.Context
+import androidx.room.Room
+import com.example.elifmigrationapp.MusicDatabase
 import com.example.elifmigrationapp.Song
 
 class Datasource {
-    fun loadDataFromDatabase(): List<Song> {
-        return listOf(
-            Song(1, "Song Name1", 1, 1, 1),
-            Song(2, "Song Name2", 2, 2, 2)
+    fun loadDataFromDatabase(db: MusicDatabase): List<Song> {
+        val dao = db.musicDao()
+
+        // Adding the last column as we finish part 2
+        val songs = listOf(
+            Song(1, "Tell Me Baby", 3, 3, 3),
+            Song(2, "Hysteria", 2, 2, 2),
+            Song(3, "White Shadows", 3, 1, 1)
         )
+        dao.insertAllSongs(songs)
+
+        return dao.getAllSongs()
     }
 }
